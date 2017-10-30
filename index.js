2da125af1cba9cf6ad75e02c0e116014175d5bad
chandrashekar869 <shekard.chandra869@gmail.com>
2017-10-30 16:55:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,6 +41,15 @@ app.get("/",function(req,res){
 app.post("/medlynkdevicelistener",function(req,res){
     console.log("req",req.body.message);
     res.send(req.body);
+    if(message.split("&&")[2]==0){
+        //login message
+        console.log("Login message");
+        res.send("98989898&&++&&0&&0&&0000&&1000");
+    }
+    if(message.split("&&")[2]==1){
+    res.send("98989898&&++&&1&&0&&0001&&1001&&++&&2&&relay1=1&&0001&&++&&2&&relay2=1&&0001&&++&&2&&config=/server/path&&0001");
+    console.log("Periodic message");
+    }
     //breakdown=message.split(dlmPattern);
     /*
     if(message.split("&&")[2]==0){
