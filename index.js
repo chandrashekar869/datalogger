912e067673f44e43b9df357f8e476c9834960504
chandrashekar869 <shekard.chandra869@gmail.com>
2017-11-16 12:55:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,10 +33,10 @@ const app=express();
 app.use(body_parser.urlencoded({
     extended:true
 }));
-/*
+
 var http=require('http');
 var qs = require('querystring');
-http.createServer(function(req,res){
+/*http.createServer(function(req,res){
     res.writeHead(200,{'content-type':'text/plain'});
         res.write("Hello");
     res.end("End");
@@ -62,17 +62,17 @@ app.get("/",function(req,res){
  }); 
 app.post("/",function(req,res){
     res.writeHead(200,{'content-type':'text/plain'});
-        res.end("98989898&&++&&0&&0&&0000&&1000");
+        res.send("98989898&&++&&0&&0&&0000&&1000");
         var body = '';
-        request.on('data', function (data) {
+        req.on('data', function (data) {
             body += data;
             // Too much POST data, kill the connection!
             // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
             console.log(body);
             if (body.length > 1e6)
-                request.connection.destroy();
+                req.connection.destroy();
         });
-        request.on('end', function () {
+        req.on('end', function () {
             var post = qs.parse(body);
             // use post['blah'], etc.
         });
