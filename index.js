0b2d4fb3243faeee25e2a02e9ff9d695bd70fad0
chandrashekar869 <shekard.chandra869@gmail.com>
2017-11-16 12:48:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ var http=require('http');
 var qs = require('querystring');
 http.createServer(function(req,res){
     res.writeHead(200,{'content-type':'text/plain'});
-        res.end("Hello");
+        res.write("Hello");
         var body = '';
         req.on('data', function (data) {
             body += data;
