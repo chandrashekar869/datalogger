332e8cab587664e54e5066f3894c190b0b708aad
chandrashekar869 <shekard.chandra869@gmail.com>
2017-11-16 12:21:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,31 +34,25 @@ app.use(body_parser.urlencoded({
     extended:true
 }));
 
-var net = require('net');
+var http=require('http');
+http.createServer(function(req,res){
+    if (request.method == 'POST') {
+        var body = '';
+        request.on('data', function (data) {
+            body += data;
+            // Too much POST data, kill the connection!
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            console.log(body);
+            if (body.length > 1e6)
+                request.connection.destroy();
+        });
+        request.on('end', function () {
+            var post = qs.parse(body);
+            // use post['blah'], etc.
+        });
+    }
+}).listen(3000);
 
-var host = '40.71.199.63',
-    port = 3000,
-    socket = net.connect(port, host, function() {
-
-    var request = "POSTs / HTTP/1.1\r\nHost: " + host + "\r\n\r\n",
-        rawResponse = "";
-
-    // send http request:
-    socket.end(request);
-
-    // assume utf-8 encoding:
-    socket.setEncoding('utf-8');
-
-    // collect raw http message:
-    socket.on('data', function(chunk) {
-        rawResponse += chunk;
-    });
-    socket.on('end', function(){
-        console.log(rawResponse);
-    });
-
-
-});
 /*app.get("/",function(req,res){
     console.log("Server running");
     res.sendFile("core.html",{root:__dirname});
