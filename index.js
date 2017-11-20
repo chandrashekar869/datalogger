ce3c855e4f2a497fdbca35afc223e3f789cb03bf
chandrashekar869 <shekard.chandra869@gmail.com>
2017-11-20 13:41:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 
 const express=require('express');
-const body_parser=require('body-parser');
 const app=express();
-var http=require('http');
 var qs=require('querystring');
 var message='';
-app.use(body_parser.urlencoded({
-    extended:true
-}));
 app.post("/",function(req,res){
     console.log("Started");
     res.send("Hai");
@@ -20,26 +15,11 @@ app.post("/",function(req,res){
             
             if (body.length > 1e6)
                 req.connection.destroy();
-        
             console.log(body);
-    //       console.log("req",body);
-      //message=body;
         }); 
         req.on('end', function () {
           var post = qs.parse(body);
         console.log(body);
-        /*if(message.split("&&")[2]==0){
-                //login message
-                console.log("Login message");  
-            res.setHeader('Content-Type', 'text/plain');
-            res.send("98989898&&++&&0&&0&&0000&&0000");
-            }
-            if(message.split("&&")[2]==1){
-            res.setHeader('Content-Type', 'text/plain');
-            res.send("98989898&&++&&1&&0&&0001&&1001&&++&&2&&relay1=1&&0001&&++&&2&&relay2=1&&0001&&++&&2&&config=/server/path&&0001");
-            console.log("Periodic message");
-            }*/
-            //console.log(body);
-            // use post['blah'], etc.
         });
-}).listen(3000);
+});
+app.listen(3000);
