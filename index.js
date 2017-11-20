c2eed1dab9de35df4b75faf5469b9cf7e80f2aea
chandrashekar869 <shekard.chandra869@gmail.com>
2017-11-20 13:46:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,19 @@ app.post("/",function(req,res){
             if (body.length > 1e6)
                 req.connection.destroy();
             console.log(body);
-        }); 
+        message=body;    
+    }); 
         req.on('end', function () {
           var post = qs.parse(body);
-        console.log(body);
+        if(message.split("&&")[2]==0){
+            //login message
+            console.log("Login message");
+            res.send("98989898&&++&&0&&0&&0000&&0000");
+        }
+        if(message.split("&&")[2]==1){
+        res.send("98989898&&++&&1&&0&&0001&&1001&&++&&2&&relay1=1&&0001&&++&&2&&relay2=1&&0001&&++&&2&&config=/server/path&&0001");
+        console.log("Periodic message");
+        }    
         });
 });
 app.listen(3000);
