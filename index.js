515629b47cac7a3ce19199ebfdbbcc16df8a0e2e
chandrashekar869 <shekard.chandra869@gmail.com>
2017-11-22 16:14:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,16 +40,28 @@ http.createServer("/",function(req,res){
     app.listen(3000);
 */
 
+var express        =         require("express");
+var bodyParser     =         require("body-parser");
+var app            =         express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-
-
-var express = require('express')
-var app = express()
-app.post('/', function (req, res) {
-  res.send('POST request to the homepage')
+app.get('/',function(req,res){
+  res.sendfile("index.html");
+});
+app.post('/login',function(req,res){
+  var user_name=req.body.user;
+  var password=req.body.password;
+  console.log("User name = "+user_name+", password is "+password);
+  res.end("yes");
+});
+app.listen(3200,function(){
+  console.log("Started on PORT 3000");
 })
-    app.listen(3200);
+
+
+
 
 
 
