697f69d996cca3061c69249f8267d104dccba35f
chandrashekar869 <shekard.chandra869@gmail.com>
2017-11-22 16:46:03 +0530

    Update newperiodic.js

--- newperiodic.js
+++ newperiodic.js
@@ -1,6 +1,13 @@
 var express        =         require("express");
 var bodyParser     =         require("body-parser");
 var app            =         express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/',function(req,res){
+  res.sendfile("index.html");
+});
 app.post('/',function(req,res){
   res.end("yes");
 });
