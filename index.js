086fdc752e52e8024e8be56675585f1cda5d8f08
chandrashekar869 <shekard.chandra869@gmail.com>
2017-11-22 16:51:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,8 +44,6 @@ var express        =         require("express");
 var bodyParser     =         require("body-parser");
 var app            =         express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.post('/',function(req,res){
   res.end("yes");
