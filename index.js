b53f6142e26177282b711d104cb019dda0c6344d
chandrashekar869 <shekard.chandra869@gmail.com>
2017-11-22 17:08:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -60,6 +60,18 @@ app.listen(3200,function(){
 */
 var http=require('http');
 http.createServer(function(req,res){
+     var body='';
+    req.on('data', function (data) {
+        console.log("request");
+            body += data;
+            // Too much POST data, kill the connection!
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            
+            if (body.length > 1e6)
+                req.connection.destroy();
+            console.log(body);
+        message=body;    
+    }); 
 res.writeHead(200,{'Content-type':'text/plain'});
     res.end("hello");
 }).listen(3200);
