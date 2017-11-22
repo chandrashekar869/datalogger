1569a1a39917ae6c93dbdf548873b2b5c2d7fc90
chandrashekar869 <shekard.chandra869@gmail.com>
2017-11-22 16:49:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -47,13 +47,7 @@ var app            =         express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/',function(req,res){
-  res.sendfile("index.html");
-});
 app.post('/login',function(req,res){
-  var user_name=req.body.user;
-  var password=req.body.password;
-  console.log("User name = "+user_name+", password is "+password);
   res.end("yes");
 });
 app.listen(3200,function(){
