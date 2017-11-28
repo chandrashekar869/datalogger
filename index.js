c7dd0dfd4eb88608b69c72e3e3cbc361d1f20552
chandrashekar869 <shekard.chandra869@gmail.com>
2017-11-28 10:44:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,16 +44,16 @@ app.post("/",function(req,res){
         if(message.split("&&")[2]==0){
             //login message
             console.log("Login message");
-            console.log("Login response=35353535&&++&&0&&0&&0000&&1000");
-         res.end("35353535&&++&&0&&0&&0000&&1000");
+            console.log("Login response=45454545&&++&&0&&0&&0000&&1000");
+         res.end("45454545&&++&&0&&0&&0000&&1000");
         }
         if(message.split("&&")[2]==1){
             console.log("Periodic message");
             var split=message.split("&&");
             var transid=split[5];
             console.log("transid:"+transid);
-            console.log("response=35353535&&++&&1&&0&&"+transid+"&&1001");
-            res.end("35353535&&++&&1&&0&&"+transid+"&&1001");
+            console.log("response=45454545&&++&&1&&0&&"+transid+"&&1001");
+            res.end("45454545&&++&&1&&0&&"+transid+"&&1001");
         }    
         });
  });
